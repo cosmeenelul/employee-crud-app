0fdd0697115cf978e2cffe684d107d5222c36342
cosmeenelul <cosmi.gabriel212004@gmail.com>
2025-03-02 17:23:03 +0200

    Initial commit

---
+++ employees-service/src/main/java/com/example/springboot/employees_service/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.springboot.employees_service.dao;
+
+import com.example.springboot.employees_service.enity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
